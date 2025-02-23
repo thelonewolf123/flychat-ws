799a3a55abf32b7a605131eb115ecc17527956f5
thelonewolf123 <harishkumar.vellore@gmail.com>
2025-02-23 14:24:04 +0530

    feat: index.html

--- public/client.js
+++ public/client.js
@@ -1,5 +1,13 @@
 'use strict'
 var socket = null;
+var name = null;
+
+function generateName() {
+  const adjectives = ['Happy', 'Clever', 'Swift', 'Bright', 'Kind', 'Brave'];
+  const nouns = ['Penguin', 'Dolphin', 'Fox', 'Owl', 'Lion', 'Eagle'];
+  const randomNum = Math.floor(Math.random() * 1000);
+  return `${adjectives[Math.floor(Math.random() * adjectives.length)]}${nouns[Math.floor(Math.random() * nouns.length)]}${randomNum}`;
+}
 
 function connect() {
   var serverUrl;
@@ -11,26 +19,36 @@ function connect() {
   }
 
   serverUrl = `${scheme}://${location.hostname}:${location.port}`;
-
   socket = new WebSocket(serverUrl, 'json');
 
+  // Generate and set name when connecting
+  name = generateName();
+  $('#n').val(name);
+  $('#n').prop('disabled', true);
+  $('#n').css('background', 'grey');
+  $('#n').css('color', 'white');
+
   socket.onmessage = event => {
-    const msg = JSON.parse(event.data)
-    $('#messages').append($('<li>').text(msg.name + ':' + msg.message))
+    const msg = JSON.parse(event.data);
+    const latency = Date.now() - msg.timestamp;
+    $('#messages').append($('<li>').text(
+      `${msg.name}: ${msg.message} (latency: ${latency}ms)`
+    ));
     window.scrollTo(0, document.body.scrollHeight);
   }
   $('form').submit(sendMessage);
 }
 
 function sendMessage() {
-  name = $('#n').val();
   if (name == '') {
     return;
   }
-  $('#n').prop('disabled', true);
-  $('#n').css('background', 'grey');
-  $('#n').css('color', 'white');
-  const msg = { type: 'message', name: name, message: $('#m').val() };
+  const msg = {
+    type: 'message',
+    name: name,
+    message: $('#m').val(),
+    timestamp: Date.now()
+  };
   socket.send(JSON.stringify(msg));
   $('#m').val('');
   return false;
