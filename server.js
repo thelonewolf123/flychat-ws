0fc482646b8f3586872ba1f481679cf88ff2cf52
thelonewolf123 <harishkumar.vellore@gmail.com>
2025-02-23 13:51:51 +0530

    feat: added host name

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-const { createServer }  = require('http');
+const { createServer } = require('http');
 const express = require('express');
-const WebSocket=require('ws');
-const app=express();
-app.use(express.json({ extended: false}));
+const WebSocket = require('ws');
+const app = express();
+app.use(express.json({ extended: false }));
 app.use(express.static('public'));
 
 var port = process.env.PORT || 3000;
-const server=new WebSocket.Server({ server:app.listen(port) });
+const server = new WebSocket.Server({
+  server: app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running x http://0.0.0.0:${port}`);
+  })
+});
 
 server.on('connection', (socket) => {
   socket.on('message', (msg) => {
-    server.clients.forEach( client => {
+    server.clients.forEach(client => {
       client.send(msg);
     })
   });
